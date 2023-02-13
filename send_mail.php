9ac6238ef6fa157af16e165474c29e15241aaec9
BrianAlfaro05 <brianalfaro525@gmail.com>
2023-02-13 00:47:13 -0500

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Email Setter
+*/
+$webmaster_email = "carmelita.flowers188@gmail.com";
+
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+$contact = "contact.html";
+$error_page = "error.html";
+$thankyou_page = "thank_you.html";
+
+/*
+Fields into variables
+*/
+$email_address = $_REQUEST['email'] ;
+$message = $_REQUEST['message'] ;
+$name = $_REQUEST['name'] ;
+$msg = 
+"First Name: " . $name . "\r\n" . 
+"Email: " . $email_address . "\r\n" . 
+"Message: " . $message ;
+
+
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+// If the user tries to access this script directly, redirect them to the feedback form,
+if (!isset($_REQUEST['email_address'])) {
+header( "Location: $contact" );
+}
+
+// If the form fields are empty, redirect to the error page.
+elseif (empty($first_name) || empty($email_address)) {
+header( "Location: $error_page" );
+}
+
+/* 
+If email injection is detected, redirect to the error page.
+If you add a form field, you should add it here.
+*/
+elseif ( isInjected($email_address) || isInjected($name)  || isInjected($message) ) {
+header( "Location: $error_page" );
+}
+
+// If we passed all previous tests, send the email then redirect to the thank you page.
+else {
+
+	mail( "$webmaster_email", "Feedback Form Results", $msg );
+
+	header( "Location: $thankyou_page" );
+}
+?>
